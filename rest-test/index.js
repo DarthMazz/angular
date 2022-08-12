a39e3a229cebe751dd5f69aeae9e9058b8781e1d
yo4taka.ma2moto <yo4taka.ma2moto@gmail.com>
2022-08-12 11:03:41 +0900

    add test rest server

---
+++ rest-test/index.js
@@ -0,0 +1,52 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+app.listen(8888);
+app.use(bodyParser.json());
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+let users = [
+  { id: 1, name: 'Yamada', email: 'yamada@example.com' },
+  { id: 2, name: 'Suzuki', email: 'suzuki@example.com' },
+  { id: 3, name: 'Tanaka', email: 'tanaka@example.com' },
+];
+
+app.get('/users', function(req, res) {
+  res.send(JSON.stringify(users));
+});
+
+app.post('/users', function(req, res) {
+  users.push(req.body);
+  res.end();
+});
+
+app.get('/users/:id', function(req, res) {
+  for (let i = 0; i < users.length; i++) {
+    if (users[i].id == req.params.id) {
+      res.send(JSON.stringify(users[i]))
+    }
+  }
+  res.end();
+});
+
+app.post('/users/:id', function(req, res) {
+  for (let i = 0; i < users.length; i++) {
+    if (users[i].id == req.params.id) {
+      users[i] = req.body;
+    }
+  }
+  res.end();
+});
+
+app.delete('/users/:id', function(req, res) {
+  for (let i = 0; i < users.length; i++) {
+    if (users[i].id == req.params.id) {
+      users.splice(i, 1);
+    }
+  }
+  res.end();
+});
